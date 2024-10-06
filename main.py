e6279ec88cd1715ad4abc767ccc8a4d8e9be202b
ivan <oooivanlau2@gmai.com>
2024-10-06 10:14:38 +0300

    Add: Core and middleware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from starlette.responses import HTMLResponse
-
+from fastapi.middleware.cors import CORSMiddleware
 from src.currency.conversion import router as conversion_router
 from fastapi.templating import Jinja2Templates
 
@@ -9,6 +9,23 @@ templates = Jinja2Templates(directory="templates")
 app = FastAPI(
     title='Currency Conversion',
 
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    # port for react
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
