0759af2ac1397887696fa75d967b754c3a3fd945
OnnnyxXx <oooivanlau2@gmail.com>
2024-10-11 20:25:54 +0300

    UPD: main.py UPD: index.html

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Request
-from starlette.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
+from starlette.responses import HTMLResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+import requests
+from config.currency_api import curr_api
 from src.currency.conversion import router as conversion_router
 from fastapi.templating import Jinja2Templates
 
@@ -10,6 +12,8 @@ app = FastAPI(
     title='Currency Conversion',
 
 )
+
+app.include_router(conversion_router)
 
 origins = [
     "http://localhost",
@@ -31,7 +35,36 @@ app.add_middleware(
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    return templates.TemplateResponse(request=request, name="index.html")
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-app.include_router(conversion_router)
+@app.post("/", response_class=HTMLResponse)
+async def currency_conversion(request: Request, from_: str = Form(...), to: str = Form(...), amount: str = Form(...)):
+    """
+    :param from_:
+        This option is intended for the currency we are converting.
+
+    :param to:
+        This is the volute we are converting to.
+
+    :param amount:
+        Number of currencies convertible.
+
+    :return:
+        json response
+    """
+    try:
+        url = f"https://api.apilayer.com/currency_data/convert?to={to}&from={from_}&amount={amount}"
+
+        payload = {}
+        headers = {
+            "apikey": curr_api
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        status_code = response.status_code
+        result = response.json()
+        print(result)
+
+        return templates.TemplateResponse("index.html", {"request": request, "result": result, "status": status_code})
+    except Exception as error:
+        return templates.TemplateResponse("index.html", {"request": request, "error": str(error)})
--- src/currency/conversion.py
+++ src/currency/conversion.py
@@ -1,7 +1,5 @@
 import requests
 from fastapi import APIRouter, Request
-from starlette.responses import HTMLResponse
-
 from config.currency_api import curr_api
 from src.currency.models import CurrencyParameter
 from fastapi.templating import Jinja2Templates
@@ -12,43 +10,6 @@ router = APIRouter(
     tags=['Currency Conversion']
 
 )
-
-
-@router.get('/currency/')
-async def currency_conversion(request: Request, from_: str, to: str, amount: str):
-    """
-    :param from_:
-        This option is intended for the currency we are converting.
-
-    :param to:
-        This is the volute we are converting to.
-
-    :param amount:
-        Number of currencies convertible.
-
-    :return:
-        json response
-    """
-    try:
-        url = f"https://api.apilayer.com/currency_data/convert?to={to}&from={from_}&amount={amount}"
-
-        payload = {}
-        headers = {
-            "apikey": curr_api
-        }
-        response = requests.request("GET", url, headers=headers, data=payload)
-        status_code = response.status_code
-        result = response.json()
-        print(result)
-        return templates.TemplateResponse(
-            request=request, name="index.html",
-            context={"result": result},
-
-        )
-
-        # return {'Result': result, 'Status': status_code}
-    except Exception as error:
-        return {'error': str(error)}
 
 
 @router.post('/api/v1/currency/')
